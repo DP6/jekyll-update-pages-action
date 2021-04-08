c2529d1350340da0a8051843675f4e8540bdade1
Michael Currin <18750745+MichaelCurrin@users.noreply.github.com>
2021-04-08 09:32:43 +0200

    refactor: :hammer: Update entrypoint.sh for readability, consistency and shell style

--- entrypoint.sh
+++ entrypoint.sh
@@ -17,20 +17,24 @@ username=$1
 token=$2
 filter=$3
 
-# List of all repositories with org = DP6 and public
-output=$(curl -H "Accept: application/vnd.github.v3+json" https://api.github.com/search/repositories?q="${filter}" | jq '.[]' | jq '[.[] | select(.homepage != null) | {name: .name, homepage:.homepage}]')
+# List of all repositories matching filter.
+repos_response=$(curl "https://api.github.com/search/repositories?q=${filter}" -H "Accept: application/vnd.github.v3+json")
+output=$(echo "$repos_response" | jq '.[]' | jq '[.[] | select(.homepage != null) | {name: .name, homepage:.homepage}]')
 
-echo $output | jq '.'
+echo "$output" | jq '.'
+
+_jq() {
+  echo "$row" | base64 --decode | jq -r "$1"
+}
 
 for row in $(echo "${output}" | jq -r '.[] | @base64'); do
-  _jq() {
-    echo ${row} | base64 --decode | jq -r ${1}
-  }
-
   repo=$(_jq '.name')
 
   echo "Update gh-pages $repo"
-  # Deploy Github pages
-  response=$(curl -u $username:$token -X POST https://api.github.com/repos/dp6/$repo/pages/builds -H "Accept: application/vnd.github.mister-fantastic-preview+json")
-  echo $response | jq '.'
+  # Deploy GitHub pages
+  response=$(curl -u "$username:$token" \
+    -X POST "https://api.github.com/repos/dp6/$repo/pages/builds" \
+    -H "Accept: application/vnd.github.mister-fantastic-preview+json" \
+  )
+  echo "$response" | jq '.'
 done
